b04e1b66f5a24524b364cb48163cf808148ea9aa
p7m <perpetuum7@protonmail.com>
2022-06-07 11:29:11 +0200

    feat: typechain hooks

---
+++ packages/react-query-typechain/src/index.ts
@@ -0,0 +1,10 @@
+export * from './base/TransactionError';
+export * from './base/TransactionStatus';
+export * from './base/Unpacked';
+
+export * from './utils/matchSmartContractReadCallQuery/matchSmartContractReadCallQuery';
+export * from './hooks/useSmartContractEvents/useSmartContractEvents';
+export * from './hooks/useSmartContractReadCall/useSmartContractReadCall';
+export * from './hooks/useSmartContractReadCalls/useSmartContractReadCalls';
+export * from './hooks/useSmartContractTransaction/useSmartContractTransaction';
+export * from './types';
---
+++ packages/react-query-typechain/src/utils/matchSmartContractReadCallQuery/matchSmartContractReadCallQuery.ts
@@ -0,0 +1,18 @@
+import type { Contract } from 'ethers';
+import isEqual from 'lodash.isequal';
+import type { Query } from 'react-query';
+import { makeSmartContractReadCallQueryKey } from '../../hooks/useSmartContractReadCall/useSmartContractReadCall';
+import type { ContractMethodName } from '../../types';
+
+/**
+ * Utility for matching smart contract read call queries when busting the cache.
+ */
+export function matchSmartContractReadCallQuery<TContract extends Contract, TMethodName extends ContractMethodName<TContract>>(
+	query: Query,
+	contractAddress: string | undefined,
+	methodName: TMethodName,
+	callArgs: Parameters<TContract['functions'][TMethodName]> | undefined
+): boolean {
+	const match = isEqual(query.queryKey, makeSmartContractReadCallQueryKey(contractAddress, methodName, callArgs));
+	return match;
+}
