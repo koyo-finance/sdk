7d028c5c50bf16483fccffe2ee13cbffeb2d74bc
p7m <perpetuum7@protonmail.com>
2022-06-08 12:06:01 +0200

    feat: default bignumber return

--- packages/react-query-typechain/src/index.ts
+++ packages/react-query-typechain/src/index.ts
@@ -3,8 +3,12 @@ export * from './base/TransactionStatus';
 export * from './base/Unpacked';
 
 export * from './utils/matchSmartContractReadCallQuery/matchSmartContractReadCallQuery';
+export * from './utils/defaultValues';
+export * from './utils/isOverridesObject';
+
 export * from './hooks/useSmartContractEvents/useSmartContractEvents';
 export * from './hooks/useSmartContractReadCall/useSmartContractReadCall';
 export * from './hooks/useSmartContractReadCalls/useSmartContractReadCalls';
 export * from './hooks/useSmartContractTransaction/useSmartContractTransaction';
+
 export * from './types';
