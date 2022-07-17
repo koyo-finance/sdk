6837ba6fa505810322e06cabc758d8ce62cddf8e
p7m <perpetuum7@protonmail.com>
2022-07-17 09:25:35 +0200

    feat: enum with block subgraphs

---
+++ packages/core-sdk/src/constants/subgraphs.ts
@@ -0,0 +1,13 @@
+import { ChainId } from '@koyofinance/core-sdk';
+
+export const BOBA_BLOCKS_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/koyo-finance/boba-blocks';
+export const AURORA_BLOCKS_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/koyo-finance/aurora-blocks';
+export const MOONRIVER_BLOCKS_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/koyo-finance/moonriver-blocks';
+export const POLYGON_BLOCKS_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/koyo-finance/matic-blocks';
+
+export const CHAIN_BLOCKS_SUBGRAPH: { [C in ChainId]?: string } = {
+	[ChainId.BOBA]: BOBA_BLOCKS_SUBGRAPH_URL,
+	[ChainId.AURORA]: AURORA_BLOCKS_SUBGRAPH_URL,
+	[ChainId.MOONRIVER]: MOONRIVER_BLOCKS_SUBGRAPH_URL,
+	[ChainId.POLYGON]: POLYGON_BLOCKS_SUBGRAPH_URL
+};
